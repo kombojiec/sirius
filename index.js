fcb84bf3a6b71d3596659064aa8e1f202da1521d
Kulakov Pavel <kombojiec@gmail.com>
2021-05-18 16:18:48 +0300

    feat(master): create page

---
+++ index.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function(){ 
+  "use strict";
+
+  $('.slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          // arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
+  // <<============== document's end =================>>
+});
